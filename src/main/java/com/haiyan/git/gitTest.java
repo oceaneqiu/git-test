8b1b9b3c9be2432bf65a8f3fb10e9f0e8a99d66d
Haiyan <oceaneqiu@gmail.com>
2022-05-14 12:47:01 +0200

    second commit

--- src/main/java/com/haiyan/git/gitTest.java
+++ src/main/java/com/haiyan/git/gitTest.java
@@ -3,5 +3,6 @@ package com.haiyan.git;
 public class gitTest {
     public static  void main(String[] args){
         System.out.println("Hello git!");
+        System.out.println("Hello git! 2");
     }
 }
